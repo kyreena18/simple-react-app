478dc894188ada5f71fafaf85fa4e873e4147c2a
Kyreena Daruwalla <157062399+kyreena18@users.noreply.github.com>
2026-03-10 00:17:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-import { useState } from "react";
-
 function App() {
-
-  const [num1, setNum1] = useState("");
-  const [num2, setNum2] = useState("");
-  const [result, setResult] = useState("");
-
-  const addNumbers = () => {
-    const sum = Number(num1) + Number(num2);
-    setResult(sum);
-  };
-
   return (
-    <div style={{textAlign:"center", marginTop:"50px"}}>
-      <h2>Add Two Numbers</h2>
-
-      <input
-        type="number"
-        placeholder="Enter first number"
-        value={num1}
-        onChange={(e)=>setNum1(e.target.value)}
-      />
-
-      <br/><br/>
-
-      <input
-        type="number"
-        placeholder="Enter second number"
-        value={num2}
-        onChange={(e)=>setNum2(e.target.value)}
-      />
-
-      <br/><br/>
-
-      <button onClick={addNumbers}>Add</button>
-
-      <h3>Result: {result}</h3>
-
+    <div>
+      <h1>Hello World</h1>
+      <p>My first React application</p>
     </div>
   );
 }
